c9504026feee350d53c20d79e9cf8a441b3298f5
roc102 <mdfahadrahman1969@gmail.com>
2024-03-07 21:45:59 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500 min-h-screen flex items-center justify-center">
+      <div className="text-white text-center">
+        <h1 className="text-4xl font-bold mb-4">
+          Competitive Examination Platform
+        </h1>
+        <p className="text-lg">Click the Start Button to Begin the Exam</p>
+        <button className="mt-8 bg-white text-black py-2 px-4 rounded-md shadow-md hover:shadow-lg focus:outline-none">
+          Start Exam
+        </button>
+      </div>
     </div>
   );
 }
